addf01a0222d548deb7c579d3d859f60edd21a27
Vlad Panin <vladtayaki@gmail.com>
2021-03-07 21:43:43 +0300

    CheckEng | added gtContext

---
+++ checkeng/context.h
@@ -0,0 +1,27 @@
+#include <QObject>
+
+/** \namespace gt */
+namespace gt {
+
+    template <class T>
+    inline void removeChildren(QObject* parent) {
+        while(auto w = parent->findChild<T>())
+            delete w;
+    }
+    inline void removeChildren(QObject* parent) {
+        while(auto w = parent->findChild<QObject*>())
+            delete w;
+    }
+    /**
+     *  gt::content::create<DigitalStationForm>(ui, ui->w_content);
+     *  gt::content::create(ui, ui->w_content);
+    **/
+    template<class T>
+    inline void create(T*& ui, QWidget* content) {
+        gt::content::removeChildren(content);
+        delete ui;
+        ui = nullptr;
+        ui = new T{};
+        ui->setupUi(content);
+    }
+}  // end namespace gt
