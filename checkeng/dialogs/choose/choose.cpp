401b26e886db0e974bc3f412f3c28bbfc7326a6f
Vlad Panin <vladtayaki@gmail.com>
2021-03-22 21:14:07 +0300

    CheckEng | added toolbuttons on choose

--- checkeng/dialogs/choose/choose.cpp
+++ checkeng/dialogs/choose/choose.cpp
@@ -1,5 +1,8 @@
 ﻿#include <QDirIterator>
 #include <QDebug>
+#include <QToolButton>
+#include <QInputDialog>
+#include <QMessageBox>
 #include "choose.h"
 #include "include/program.h"
 #include "ui_choose.h"
@@ -27,12 +30,38 @@ void ChooseDialog::loadFiles() noexcept
 {
     QStringList filter;
     QDirIterator it(program::paths::test1(), filter, QDir::AllEntries | QDir::NoSymLinks | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
-    QStringList files;
-    while (it.hasNext())
-        files << it.next();
-//    if (!text.isEmpty())
-//        files = findFiles(files, text);
-//    files.sort();
-//    showFiles(files)
-    qDebug() << files;
+    int cx = 1;
+    while (it.hasNext()) {
+        auto const& name = it.next();
+        auto v = new QHBoxLayout();
+        auto btn = new QPushButton(GT_STR("Text Task %1").arg(cx++));
+        btn->setProperty("file", name);
+        v->addWidget(btn);
+
+        auto btn1 = new QToolButton();
+        btn1->setText(GT_STR("Edit"));
+        QObject::connect(btn1, &QToolButton::clicked, [cx, name] () {
+            bool ok;
+            auto const& text = QInputDialog::getText(nullptr, program::fullProductName(),
+                                                      tr("Enter a new name:"), QLineEdit::Normal,
+                                                      GT_STR("Text Task %1").arg(cx), &ok);
+            if (ok && !text.isEmpty())
+                QFile::rename(name, GT_STR("%1/%2").arg(QFileInfo(name).absolutePath()).arg(text));
+        });
+        v->addWidget(btn1);
+
+        auto btn2 = new QToolButton();
+        btn2->setText(GT_STR("Delete"));
+        QObject::connect(btn2, &QToolButton::clicked, [name] () {
+            int ret = QMessageBox::information(nullptr, program::fullProductName(),
+                                           tr("Do you really want to delete test?"),
+                                            QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+            if (ret == QMessageBox::Yes)
+                QFile::remove(name);
+        });
+        v->addWidget(btn2);
+
+        ui->vl_2->addLayout(v);
+    }
+
 }
